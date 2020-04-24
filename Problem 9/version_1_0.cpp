5ccaeca4b24a206fc74b1c543a5a7150f8b68c9a
Minho Choi <minhochoi.official@gmail.com>
2020-04-24 10:14:30 -0700

    Updated Problem 10

---
+++ Problem 9/version_1_0.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+int solve(double n) {
+    for (int a = 1; a < n/3; a++) {
+        for (int b = a+1; b < (n-a)/2; b++) {
+            int c = n-a-b;
+            if ((a*a + b*b) == c*c) {
+                return a*b*c;
+            }
+        }
+    }
+
+    return 0;
+}
+
+
+int main() {
+    cout << solve(1000) << endl;
+
+    return 0;
+}
