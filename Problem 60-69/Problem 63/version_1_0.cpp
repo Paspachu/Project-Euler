2d8a9c4622acceb3499be83e9c2a8babffeb2753
Minho Choi <minhochoi.official@gmail.com>
2020-06-12 14:39:47 +0900

    Updated Problem 63

---
+++ Problem 60-69/Problem 63/version_1_0.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include "vector_int.h"
+using namespace std;
+
+
+int solve(int b) {
+    int count = 0;
+    vector<int> n;
+    vector<int> vone = {1};
+
+    for (int i = 1; i < b; i++) {
+        n = {1};
+
+        while (pow(n, i) < pow({0, 1}, i)) {
+            if (pow(n, i).size() == i) {
+                print(pow(n, i));
+                count++;
+            }
+
+            n = n + vone;
+        }
+
+        cout << endl;
+    }
+
+    return count;
+}
+
+
+
+int main() {
+    cout << solve(22) << endl;
+
+    return 0;
+}
